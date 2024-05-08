351f6860cda15ed19f251fbd9894cebf7243ecc0
j-lgrant <justingrant373@gmail.com>
2024-05-08 17:29:24 +0100

    http methods

---
+++ Members.js
@@ -0,0 +1,24 @@
+
+const members = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'john.doe@gmail.com',
+    status: 'inactive'
+  },
+  {
+    id: 2,
+    name: 'robbie Doe',
+    email: 'robbie.doe@gmail.com',
+    status: 'active'
+  },
+  {
+    id: 3,
+    name: 'RDoe',
+    email: 'rDoe@gmail.com',
+    status: 'active'
+  }
+
+]
+
+module.exports = members
